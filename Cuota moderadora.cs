ac15616f4f5df9e36d0fb1299f1ac9da425f5729
Lester-Cano <53066925+Lester-Cano@users.noreply.github.com>
2019-07-25 20:10:51 -0500

    Add files via upload

---
+++ Cuota moderadora.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Programa para calcular la tarifa de la eps y la cuota moderadora con base en su salario y si posee sisben");
+            Console.WriteLine("Porfavor ingrese su salaraio sin los puntos del mil");
+            int salario = int.Parse(Console.ReadLine());
+            int smmlv = 828116;
+
+            if (salario < smmlv)
+            {
+                Console.WriteLine("marque 1 si ud posee sisben, marque 2 si no");
+                int sisben = int.Parse(Console.ReadLine());
+               
+                if (sisben == 1)
+                {
+                    Console.WriteLine("Su tarifa es la A y no tiene que pagar cuota moderadora");
+                }
+                else if (sisben == 2)
+                {
+                    Console.WriteLine("Su tarifa es la A y el valor de su cuota moderadora es $ 3.200");
+                }
+            }
+            else if (salario < (2 * smmlv))
+            {
+                Console.WriteLine("Su tarifa es la A y el valor de su cuota moderadora es $ 3.200");
+            }
+            else if ((2 * smmlv) <= salario && salario <= (5 * smmlv))
+            {
+                Console.WriteLine("Su tarifa es la B y el valor de su cuota moderadora es $ 12.700");
+            }
+            else
+            {
+                Console.WriteLine("Su tarifa es la C y el valor de su cuota moderadora es $ 33.500");
+            }
+        }
+    }
+}
---
+++ Tarifa servicios.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp8
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Programa para calcular la tarifa en servicios que ud debe de pagar teniendo en cuenta su salario");
+            Console.WriteLine("Porfavor ingrese su salaraio");
+            double salario = double.Parse(Console.ReadLine());
+            double smmlv = 828116;
+            if (salario < (2 * smmlv)) 
+            {
+                Console.WriteLine("Su tarifa es la A");
+            }
+            else if ((2 * smmlv) <= salario && salario <= (4 * smmlv)) 
+            {
+                Console.WriteLine("Su tarifa es la B");
+            }
+            else
+            {
+                Console.WriteLine("Su tarifa es la C");
+            }
+        }
+    }
+}
