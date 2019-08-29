b3348bb18f2de779681565af34ec8a1872ffc418
Lester-Cano <53066925+Lester-Cano@users.noreply.github.com>
2019-08-29 09:14:12 -0500

    Add files via upload

---
+++ Arreglos menor a mayor.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main()
+        {
+
+            string[] nombres = { "s", "l", "m", "k", "m" };
+            int[] notas = { 1, 2, 1, 5, 4 };
+            string index;
+            int pos;
+
+            for (int i = 0; i < notas.Length - 1; i++)
+            {
+                for (int j = 0; j < notas.Length - 1; j++)
+                {
+                    if( notas[j+1]>notas[j])
+                    {
+                        pos = notas[j + 1];
+                        notas[j + 1] = notas[j];
+                        notas[j] = pos;
+
+                        index = nombres[j + 1];
+                        nombres[j + 1] = nombres[j];
+                        nombres[j] = index;
+                    }
+                }
+            }
+            for (int i = 0; i < notas.Length; i++)
+            {
+                Console.WriteLine("posicion: " + notas[i] +" Nombres: "+nombres[i]);
+            }
+
+        }
+    }
+}
