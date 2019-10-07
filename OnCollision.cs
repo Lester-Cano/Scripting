29f50a7e2d5f1471ede8cf1dd73417027fc41e95
Lester-Cano <53066925+Lester-Cano@users.noreply.github.com>
2019-10-07 17:42:50 -0500

    Add files via upload

---
+++ OnCollision.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Colores : MonoBehaviour
+{
+
+    Renderer mrenderer;
+    Color azul = new Color (0f, 0f, 0.85f);
+    Color morado = new Color(0.85f, 0f, 0.85f);
+    Color rojo = new Color(0.85f, 0f, 0f);
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mrenderer = GetComponent<Renderer>();
+        mrenderer.material.color = azul;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+      
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        mrenderer.material.color = morado;
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        mrenderer.material.color = rojo;
+    }
+
+}
